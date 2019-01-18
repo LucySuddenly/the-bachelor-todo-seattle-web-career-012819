f55af9b14ed9e1f09cbe7690a11d4154d84a5594
Lucy Suddenly <suddenlylucy@gmail.com>
2019-01-18 19:09:37 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -25,7 +25,17 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0 
+  data.each do |season_number, contestants|
+    contestants.each do |contestant|
+      contestant.each do |attribute, value|
+        if value == hometown
+          counter += 1
+        end
+      end 
+    end 
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
