9e44259ae1bff832ef521654588bcee25f09b9f0
Lucy Suddenly <suddenlylucy@gmail.com>
2019-01-18 19:02:08 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,19 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner_first_name = []
+  data.each do |season_number, contestants|
+    contestants.each do |contestant|
+      contestant.each do |attribute, value|
+        if value == "Winner" && season == season_number
+          winner_first_name = contestant["name"].split
+        end
+      end 
+    end 
+  end
+  winner_first_name[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  
 end
 
 def count_contestants_by_hometown(data, hometown)
